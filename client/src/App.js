cd4a1c5f5ba8d962ae35415e56713107cedb7b2a
Sehaj-kahlon <sehajkahlon437@gmail.com>
2024-07-27 21:00:57 +0530

    Post API Call

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import UseEffectCall from "./components/useEffectCall";
 import ButtonAPICall from "./components/ButtonAPICall";
-
+import POSTAPICall from "./components/POSTAPICall";
 function App() {
   return (
     <div>
       <UseEffectCall />
       <ButtonAPICall />
+      <POSTAPICall />
     </div>
   );
 }
--- flask-server/localhost.py
+++ flask-server/localhost.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app  = Flask(__name__)
@@ -13,6 +13,18 @@ def members():
     data = "1000"
     print(data)
     return jsonify({'data': data})
+@app.route("/postCall", methods = ['POST'])
+def SUM():
+    
+    try:
+        data = request.get_json()
+        numberA = data.get('numberA')
+        numberB = data.get('numberB')
+        result  =numberA + numberB
+        return jsonify({'result': result}), 200
+    except Exception as e:  
+        return jsonify({'error': str(e)}), 400
+    
 
 if __name__ == "__main__":
     app.run(debug = True)
