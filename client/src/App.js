ddb01eae7bfd0e091fbb35cfff5e11e47b4b673b
Sehaj <DELL@MacBook-Pro.local>
2024-08-10 21:25:15 +0530

    register/signUP page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import SignUp from "./components/register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <Router>
-    <div className="App">
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Routes>
-            <Route path="/register" element={<SignUp />} />
-          </Routes>
+      <div className="App">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route path="/register" element={<SignUp />} />
+            </Routes>
+            <ToastContainer />
+          </div>
         </div>
       </div>
-    </div>
-  </Router>
+    </Router>
   );
 }
 
--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -3,6 +3,8 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "./firebase";
 import { setDoc, doc } from "firebase/firestore";
 import { Form, Input } from "antd";
+import { toast } from "react-toastify"
+
 function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -14,81 +16,87 @@ function SignUp() {
       await createUserWithEmailAndPassword(auth, email, password);
       const user = auth.currentUser;
       console.log(user);
+      console.log("User Registered Successfully!!");
       if (user) {
         await setDoc(doc(db, "Users", user.uid), {
           email: user.email,
           firstName: fname,
           lastName: lname,
-          photo:""
+          createdAt: new Date(),
         });
       }
-      console.log("User Registered Successfully!!");
-    //   toast.success("User Registered Successfully!!", {
-    //     position: "top-center",
-    //   });
+      toast.success("User Registered Successfully!!", {
+        position: "top-center",
+      });
     } catch (error) {
       console.log(error.message);
-    //   toast.error(error.message, {
-    //     position: "bottom-center",
-    //   });
+      toast.error(error.message, {
+        position: "bottom-center",
+      });
     }
   };
 
   return (
-    <Form onSubmit={handleRegister}>
-      <h3>Sign Up</h3>
-      <div className="mb-3">
-        <label>First name</label>
-        <Input
-          type="text"
-          className="form-control"
-          placeholder="First name"
-          onChange={(e) => setFname(e.target.value)}
-          required
-        />
-      </div>
+    <div>
+      <Form>
+        <h3>Sign Up</h3>
+        <div className="mb-3">
+          <label>First name</label>
+          <Input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+            onChange={(e) => setFname(e.target.value)}
+            required
+          />
+        </div>
 
-      <div className="mb-3">
-        <label>Last name</label>
-        <Input
-          type="text"
-          className="form-control"
-          placeholder="Last name"
-          onChange={(e) => setLname(e.target.value)}
-        />
-      </div>
+        <div className="mb-3">
+          <label>Last name</label>
+          <Input
+            type="text"
+            className="form-control"
+            placeholder="Last name"
+            onChange={(e) => setLname(e.target.value)}
+          />
+        </div>
 
-      <div className="mb-3">
-        <label>Email address</label>
-        <Input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
+        <div className="mb-3">
+          <label>Email address</label>
+          <Input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
 
-      <div className="mb-3">
-        <label>Password</label>
-        <Input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <Input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
 
-      <div className="d-grid">
-        <button type="submit" className="btn btn-primary">
-          Sign Up
-        </button>
-      </div>
-      <p className="forgot-password text-right">
-        Already registered? <a href="/login">Login</a>
-      </p>
-    </Form>
+        <div className="d-grid">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleRegister}
+          >
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered? <a href="/login">Login</a>
+        </p>
+      </Form>
+    </div>
   );
 }
 
